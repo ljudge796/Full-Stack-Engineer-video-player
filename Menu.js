d1c8330d34caf24253c9723a6bd6fc7cd1f312ca
Liam Judge <ljudge2025@gmail.com>
2025-08-28 19:19:51 +0100

    Create Menu.js

---
+++ Menu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  handleClick(e) {
+    const text = e.target.value;
+    this.props.onSelectVideo(text);
+  }
+
+  render() {
+    return (
+      <form onClick={this.handleClick}>
+        <input type="radio" name="src" value="fast" disabled={this.props.currentSrc === 'fast'} /> fast
+        <input type="radio" name="src" value="slow" /> slow
+        <input type="radio" name="src" value="cute" /> cute
+        <input type="radio" name="src" value="eek" /> eek
+      </form>
+    );
+  }
+}
